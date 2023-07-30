fb7efef3a10513e7bfc270acec225025065a6fda
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-07-30 18:19:23 -0300

    Initial Commit

---
+++ FacebookLogin Clone/FacebookLogin_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookLogin_CloneApp.swift
+//  FacebookLogin Clone
+//
+//  Created by Luiz Henrique Silva Sampaio on 30/07/23.
+//
+
+import SwiftUI
+
+@main
+struct FacebookLogin_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
